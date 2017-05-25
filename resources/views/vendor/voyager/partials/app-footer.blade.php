c70dc74b2c3e1f96ef6bd7e5f1f3ed7d38627bcd
anonymoussc <50c5ac69@opayq.com>
2017-05-26 01:07:35 +0700

    Modified /app-footer

--- resources/views/vendor/voyager/partials/app-footer.blade.php
+++ resources/views/vendor/voyager/partials/app-footer.blade.php
@@ -1,9 +1,9 @@
 <footer class="app-footer">
     <div class="site-footer-right">
         @if (rand(1,100) == 100)
-            <i class="voyager-rum-1"></i> Gaboot Foundation
+            <i class="voyager-rum-1"></i> Love <3
         @else
-            Made with <i class="voyager-heart"></i> by <a href="https://github.com/gaboots/gaboot" target="_blank">Gaboot Foundation</a>
+            Made with <i class="voyager-heart"></i> by <a href="https://github.com/onsigbaar/onsigbaar" target="_blank">Onsigbaar</a>
         @endif
         @php $version = Voyager::getVersion(); @endphp
         @if (!empty($version))
