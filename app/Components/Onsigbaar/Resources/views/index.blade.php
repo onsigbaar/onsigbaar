44b3e27a3216463dfae7de2e18b6353e08fed5ac
anonymoussc <50c5ac69@opayq.com>
2017-05-25 22:51:28 +0700

    Deleted app\Components\Onsigbaar

--- app/Components/Onsigbaar/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('onsigbaar::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from component: {!! config('onsigbaar.name') !!}
-    </p>
-@stop
--- app/Components/Onsigbaar/src/Http/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-Route::group(['middleware' => 'web', 'prefix' => 'onsigbaar', 'namespace' => 'App\\Components\Onsigbaar\Http\Controllers'], function()
-{
-    Route::get('/', 'OnsigbaarController@index');
-});
