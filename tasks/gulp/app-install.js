1704568dbeeacaf7d08247da0a9caac3061802f0
anonymoussc <50c5ac69@opayq.com>
2017-06-05 14:06:30 +0700

    Modified app-install

--- tasks/gulp/app-install.js
+++ tasks/gulp/app-install.js
@@ -14,12 +14,12 @@ let tasks = {
 };
 
 function processing() {
-    gulp.src([config.baseDirectory + '/' + config.fromComponents + '/Publish/js/*.*']).pipe(gulp.dest('public/js/'));
-    gulp.src([config.baseDirectory + '/' + config.fromComponents + '/Publish/css/*.*']).pipe(gulp.dest('public/css/'));
-    gulp.src([config.baseDirectory + '/' + config.fromComponents + '/Publish/vendor/voyager/database/migrations/*.*']).pipe(gulp.dest('database/migrations/'));
-    gulp.src([config.baseDirectory + '/' + config.fromComponents + '/Publish/vendor/voyager/database/seeds/*.*']).pipe(gulp.dest('database/seeds/'));
+    gulp.src([config.baseDirectory + '/' + config.fromComponent + '/Publish/js/*.*']).pipe(gulp.dest('public/js/'));
+    gulp.src([config.baseDirectory + '/' + config.fromComponent + '/Publish/css/*.*']).pipe(gulp.dest('public/css/'));
+    gulp.src([config.baseDirectory + '/' + config.fromComponent + '/Publish/vendor/voyager/database/migrations/*.*']).pipe(gulp.dest('database/migrations/'));
+    gulp.src([config.baseDirectory + '/' + config.fromComponent + '/Publish/vendor/voyager/database/seeds/*.*']).pipe(gulp.dest('database/seeds/'));
 
-    return gulp.src([config.baseDirectory + '/' + config.fromComponents + '/Publish/storage/app/public/**/*.*']).pipe(gulp.dest('storage/app/public/'));
+    return gulp.src([config.baseDirectory + '/' + config.fromComponent + '/Publish/storage/app/public/**/*.*']).pipe(gulp.dest('storage/app/public/'));
 }
 
 module.exports = tasks;
