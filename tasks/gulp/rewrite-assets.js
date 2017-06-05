d15928a8d07b39b0c33f7231ba1cd9bdddfe9c8c
anonymoussc <50c5ac69@opayq.com>
2017-06-05 13:27:22 +0700

    New rewrite-assets

---
+++ tasks/gulp/rewrite-assets.js
@@ -0,0 +1,20 @@
+/**
+ * rewrite-assets.js
+ * Created by @anonymoussc on 6/5/2017 6:35 AM.
+ */
+
+'use strict';
+
+let gulp   = require('gulp');
+let config = require('./get-environment.js').config;
+
+
+let tasks = {
+    processing: processing
+};
+
+function processing() {
+    return gulp.src([config.baseDirectory + '/' + config.fromComponents + '/Publish/vendor/voyager/public/vendor/tcg/voyager/assets/**/**/*.*']).pipe(gulp.dest('./public/vendor/tcg/voyager/assets/'));
+}
+
+module.exports = tasks;
