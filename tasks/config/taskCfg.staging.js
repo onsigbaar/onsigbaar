a1e2207848f59c3f7af1ed779d945c8973f06ada
anonymoussc <50c5ac69@opayq.com>
2017-06-05 13:26:20 +0700

    New taskCfg.staging

---
+++ tasks/config/taskCfg.staging.js
@@ -0,0 +1,13 @@
+/**
+ * taskCfg.staging.js
+ * Created by @anonymoussc on 6/5/2017 6:38 AM.
+ */
+
+'use strict';
+
+let config = {
+    baseDirectory: 'vendor',
+    fromComponent: 'consigliere/siegnor'
+};
+
+module.exports = config;
