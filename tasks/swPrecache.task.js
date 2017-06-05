9a9b6d7f63df5b571cbe85690260fb9a84f46200
anonymoussc <50c5ac69@opayq.com>
2017-06-05 13:25:52 +0700

    Modified swPrecache.task

--- tasks/swPrecache.task.js
+++ tasks/swPrecache.task.js
@@ -1,19 +1,17 @@
-var path = require('path');
+var path       = require('path');
 var swPrecache = require('sw-precache');
+var Elixir     = require('laravel-elixir');
+var Task       = Elixir.Task;
 
-var Elixir = require('laravel-elixir');
+Elixir.extend('swPrecache', function () {
 
-var Task = Elixir.Task;
-
-Elixir.extend('swPrecache', function() {
-
-    new Task('generate-service-worker', function() {
+    new Task('generate-service-worker', function () {
 
         /*
-          always read the latest config. Allows changing
-          precache-config.json without reloading gulp
-        */
-        delete require.cache[require.resolve('./../precache-config.json')]
+         always read the latest config. Allows changing
+         precache-config.json without reloading gulp
+         */
+        delete require.cache[require.resolve('./../precache-config.json')];
         var preCacheConfig = require('./../precache-config.json');
 
         return swPrecache.write(path.join('public', 'service-worker.js'), preCacheConfig);
