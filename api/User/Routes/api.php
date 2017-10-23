d236981f118e0a3851f65bddccbc711601b60021
anonymoussc <50c5ac69@opayq.com>
2017-10-23 08:50:25 +0700

    Modified api api\User\Routes

--- api/User/Routes/api.php
+++ api/User/Routes/api.php
@@ -1,14 +1,14 @@
 <?php
 
+Route::group(['middleware' => 'api', 'prefix' => 'user', 'namespace' => 'Api\User\Http\Controllers'], function () {
+    //
+});
+
 Route::group(['middleware' => 'api', 'prefix' => '', 'namespace' => 'App\Components\Passerby\Http\Controllers\Auth'], function () {
-    Route::post('/login', 'LoginController@login');
-    Route::post('/login/refresh', 'LoginController@refresh');
+    Route::post('/login', 'AuthController@login')->name('login');
+    Route::post('/login/refresh', 'AuthController@refresh');
 });
 
 Route::group(['middleware' => 'auth:api', 'prefix' => '', 'namespace' => 'App\Components\Passerby\Http\Controllers\Auth'], function () {
-    Route::post('/logout', 'LoginController@logout');
-});
-
-Route::group(['middleware' => 'api', 'prefix' => 'user', 'namespace' => 'Api\User\Http\Controllers'], function () {
-    //
+    Route::post('/logout', 'AuthController@logout');
 });
