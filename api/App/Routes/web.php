74b6b00ab9731d0b73f8dba00e6bf56cfb80d649
anonymoussc <50c5ac69@opayq.com>
2017-10-23 08:49:27 +0700

    Modified web api\App\Routes

--- api/App/Routes/web.php
+++ api/App/Routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
 Route::group(['middleware' => 'web', 'prefix' => 'app', 'namespace' => 'Api\App\Http\Controllers'], function() {
-    //
+    Route::get('/', 'AppController@index');
 });
-
-Route::get('/', 'Api\App\Http\Controllers\AppController@index')->middleware('web');
