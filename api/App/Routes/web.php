3a7eae647cd9de8c7e705ecb8d2bd5205a2cddea
anonymoussc <50c5ac69@opayq.com>
2017-10-25 05:12:21 +0700

    Modified web api\App\Routes

--- api/App/Routes/web.php
+++ api/App/Routes/web.php
@@ -2,4 +2,5 @@
 
 Route::group(['middleware' => 'web', 'prefix' => 'app', 'namespace' => 'Api\App\Http\Controllers'], function() {
     Route::get('/', 'AppController@index');
+    Route::get('/login', 'AppController@login')->name('login');
 });
