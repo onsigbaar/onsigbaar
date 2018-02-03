e6ec3f9dca39190008ddfc3a4f0236d5ab9209d5
anonymoussc <50c5ac69@opayq.com>
2018-02-03 22:38:36 +0700

    Modified api api\User\Routes

--- api/User/Routes/api.php
+++ api/User/Routes/api.php
@@ -1,14 +1,41 @@
 <?php
+Route::group(['prefix' => 'api'], function() {
+    Route::group(['prefix' => '1'], function() {
+        Route::group(['middleware' => 'api', 'prefix' => 'user', 'namespace' => 'Api\User\Http\Controllers'], function() {
+            Route::get('/', 'UserController@get');
+            Route::post('/', 'UserController@create');
+            Route::put('/{id}', 'UserController@update');
+            Route::delete('/{id}', 'UserController@delete');
 
-Route::group(['middleware' => 'api', 'prefix' => 'user', 'namespace' => 'Api\User\Http\Controllers'], function () {
-    //
-});
+            Route::get('/', 'UserController@tests');
+        });
 
-Route::group(['middleware' => 'api', 'prefix' => '', 'namespace' => 'App\Components\Passerby\Http\Controllers\Auth'], function () {
-    Route::post('/login', 'LoginController@login');
-    Route::post('/login/refresh', 'LoginController@refresh');
-});
+        Route::group(['middleware' => 'api', 'prefix' => 'role', 'namespace' => 'Api\User\Http\Controllers'], function() {
+            Route::get('/', 'RoleController@get');
+            Route::post('/', 'RoleController@create');
+            Route::put('/{id}', 'RoleController@update');
+            Route::delete('/{id}', 'RoleController@delete');
+        });
 
-Route::group(['middleware' => 'auth:api', 'prefix' => '', 'namespace' => 'App\Components\Passerby\Http\Controllers\Auth'], function () {
-    Route::post('/logout', 'LoginController@logout');
+        Route::group(['middleware' => 'api', 'prefix' => 'permission', 'namespace' => 'Api\User\Http\Controllers'], function() {
+            Route::get('/', 'PermissionController@get');
+            Route::post('/', 'PermissionController@create');
+            Route::put('/{id}', 'PermissionController@update');
+            //Route::delete('/deletes', 'PermissionController@deleteWhereArray');
+            Route::delete('/{id}', 'PermissionController@delete');
+        });
+
+        Route::group(['middleware' => 'auth:api', 'prefix' => '', 'namespace' => 'Api\User\Http\Controllers'], function() {
+            Route::get('/intest', 'UserController@inTests');
+            Route::get('/outest', 'UserController@authTests');
+        });
+    });
+
+    Route::group(['middleware' => 'api', 'prefix' => 'login', 'namespace' => 'Api\User\Http\Controllers\Auth'], function() {
+        Route::post('/', 'LoginController@login');
+        Route::post('/refresh', 'LoginController@refresh');
+    });
+    Route::group(['middleware' => 'auth:api', 'prefix' => '', 'namespace' => 'Api\User\Http\Controllers\Auth'], function() {
+        Route::post('/logout', 'LoginController@logout');
+    });
 });
