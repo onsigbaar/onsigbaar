7afd139b5adb36f856ffad438b807bb952c0a019
anonymoussc <50c5ac69@opayq.com>
2018-02-03 22:39:49 +0700

    New UserController Api\User\Http\Controllers

--- api/User/src/Http/Controllers/UserController.php
+++ api/User/src/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -70,5 +71,18 @@ class UserController extends Controller
         ]);
 
         return 'success';
+    }
+
+    public function tests() {
+        return 'its works';
+    }
+
+    public function inTests() {
+        return 'b-baka';
+    }
+
+    public function authTests() {
+        return Auth::user();
+        //return Auth::id();
     }
 }
