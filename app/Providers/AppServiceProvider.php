04c1d589221206edbf49d9b94e27bf9138490ce8
anonymoussc <50c5ac69@opayq.com>
2018-02-08 02:08:37 +0700

    Modified AppServiceProvider App\Providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->register(\Api\App\Providers\AppServiceProvider::class);
+        $this->app->register(\Api\AppBase\Providers\AppBaseServiceProvider::class);
     }
 }
