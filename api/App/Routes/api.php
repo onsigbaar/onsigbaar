ba3d97945e48b81d1e76348393c3c1958a340d68
anonymoussc <50c5ac69@opayq.com>
2018-02-08 00:32:29 +0700

    Deleted api\App

--- api/App/Routes/api.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-Route::group(['middleware' => 'api', 'prefix' => 'app', 'namespace' => 'Api\App\Http\Controllers'], function() {
-    //
-});
--- api/App/Routes/web.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-Route::group(['middleware' => 'web', 'prefix' => 'app', 'namespace' => 'Api\App\Http\Controllers'], function() {
-    Route::get('/', 'AppController@index');
-    Route::get('/login', 'AppController@login')->name('login');
-});
