3250b8b4cf7bf7cd6d02c505fef282a8b291986f
anonymoussc <50c5ac69@opayq.com>
2018-02-11 16:52:59 +0700

    Modified signal config

--- config/signal.php
+++ config/signal.php
@@ -3,23 +3,24 @@
 return [
     'name'        => 'Signal',
 
-    // Enable logging activity
-    'logActivity' => env('LOG_ACTIVITY', true),
-
-    // Specific logging control when logActivity option enabled
-    'emergency'   => false,
-    'alert'       => false,
-    'critical'    => false,
-    'error'       => true,
-    'warning'     => false,
-    'notice'      => false,
-    'info'        => false,
-    'debug'       => false,
+    'log'   => [
+        // Enable logging activity
+        'activity'  => env('LOG_ACTIVITY', true),
+        // Specific logging control when log.activity option enabled
+        'emergency' => false,
+        'alert'     => false,
+        'critical'  => false,
+        'error'     => true,
+        'warning'   => false,
+        'notice'    => false,
+        'info'      => false,
+        'debug'     => false,
+    ],
 
     // Sent log data to email
     'email' => [
         // Set to 'true' to sent log data to email
         'sent'   => env('SIGNAL_EMAIL_SENT', false),
-        'sentTo' => env('SIGNAL_EMAIL_SENT_TO', 'example@example.io'),
+        'sentTo' => env('SIGNAL_EMAIL_SENT_TO', 'noreply@example.com'),
     ],
 ];
