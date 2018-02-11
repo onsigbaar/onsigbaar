1a2efcbe9a70d37ea3fc77ca76b3fc3db77d4199
anonymoussc <50c5ac69@opayq.com>
2018-02-12 00:27:24 +0700

    Modified app config

--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
         /*
          * Package Service Providers...
          */
+        // TCG\Voyager\VoyagerServiceProvider::class,
 
         /*
          * Application Service Providers...
