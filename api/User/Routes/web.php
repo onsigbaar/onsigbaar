04f2450a08dc84e1cf5ad9f8a74edb9e9b9f7e4a
anonymoussc <50c5ac69@opayq.com>
2018-02-11 16:46:41 +0700

    Modified web api\User\Routes

--- api/User/Routes/web.php
+++ api/User/Routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
 Route::group(['middleware' => 'web', 'prefix' => 'user', 'namespace' => 'Api\User\Http\Controllers'], function() {
-    Route::get('/data-seeder', 'UserController@dataSeeder');
-    Route::get('/abc', 'UserController@tests')->name('register');
+    //Route::get('/data-seeder', 'UserController@dataSeeder');
 });
