b86f77c0cebf95f5d8d9759d20f446ae52c8cf02
anonymoussc <50c5ac69@opayq.com>
2018-10-21 22:09:19 +0700

    Removed api/User

--- api/User/Routes/web.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-Route::group(['middleware' => 'web', 'prefix' => 'user', 'namespace' => 'Api\User\Http\Controllers'], function() {
-    //Route::get('/data-seeder', 'UserController@dataSeeder');
-});
