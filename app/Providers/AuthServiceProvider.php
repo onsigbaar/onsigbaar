5305057037458a78f25bb0e364e8dc6e087cb7a3
anonymoussc <50c5ac69@opayq.com>
2019-01-06 02:59:52 +0700

    Modified AuthServiceProvider: passport configuration

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Carbon\Carbon;
+use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +27,14 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Passport::routes(function ($router) {
+            $router->forAccessTokens();
+            $router->forPersonalAccessTokens();
+            $router->forTransientTokens();
+        });
+
+        Passport::tokensExpireIn(Carbon::now()->addMinutes(10));
+
+        Passport::refreshTokensExpireIn(Carbon::now()->addDays(10));
     }
 }
