7da1a260783a63eb527cb63fe5c584bd525e0f49
anonymoussc <50c5ac69@opayq.com>
2019-02-28 06:58:53 +0700

    feat(New database/migrations/2019_02_27_085933_add_username_to_users_table.php):
    Signed-off-by: anonymoussc <50c5ac69@opayq.com>

---
+++ database/migrations/2019_02_27_085933_add_username_to_users_table.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Copyright(c) 2019. All rights reserved.
+ * Last modified 2/28/19 2:34 AM
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUsernameToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function(Blueprint $table) {
+            $table->string('username')->after('uuid')->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function(Blueprint $table) {
+            $table->dropColumn('username');
+        });
+    }
+}
