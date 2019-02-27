69610807369a90497977721dc25d4b87470aa2cc
anonymoussc <50c5ac69@opayq.com>
2019-02-28 06:59:55 +0700

    feat(db): Modified database/seeds/UsersTableSeeder
    Signed-off-by: anonymoussc <50c5ac69@opayq.com>

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,11 @@
 <?php
+/**
+ * Copyright(c) 2019. All rights reserved.
+ * Last modified 2/28/19 6:16 AM
+ */
 
 use Illuminate\Database\Seeder;
+use Webpatser\Uuid\Uuid;
 
 class UsersTableSeeder extends Seeder
 {
@@ -19,13 +24,15 @@ class UsersTableSeeder extends Seeder
         \DB::table('users')->insert(array (
             0 =>
             array (
-                'name' => 'user',
-                'email' => 'user@api.com',
+                'uuid'              => Uuid::generate(5, 'user', Uuid::NS_DNS),
+                'username'          => 'user',
+                'name'              => 'user',
+                'email'             => 'user@api.com',
                 'email_verified_at' => NULL,
-                'password' => password_hash('user', PASSWORD_BCRYPT),
-                'remember_token' => NULL,
-                'created_at' => NULL,
-                'updated_at' => NULL,
+                'password'          => password_hash('user', PASSWORD_BCRYPT),
+                'remember_token'    => null,
+                'created_at'        => null,
+                'updated_at'        => null,
             ),
         ));
         
