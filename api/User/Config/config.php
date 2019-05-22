6da32e533a8e99aadc8a228789cbc380b889c2ae
anonymoussc <50c5ac69@opayq.com>
2019-05-22 11:06:42 +0700

    refactor(config): modified api/User/Config/config

--- api/User/Config/config.php
+++ api/User/Config/config.php
@@ -1,5 +1,17 @@
 <?php
+/**
+ * Copyright(c) 2019. All rights reserved.
+ * Last modified 5/22/19 8:45 AM
+ */
 
 return [
     'name' => 'User',
+    'api'  => [
+        'authors' => [
+            [
+                'name'  => 'anonymoussc',
+                'email' => '50c5ac69@opayq.com',
+            ],
+        ],
+    ],
 ];
