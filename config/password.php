3cbab3b6e126845baed8a4d616df3ef0e9fb9b9f
anonymoussc <50c5ac69@opayq.com>
2019-07-15 10:44:11 +0700

    refactor(config): modified config/password

--- config/password.php
+++ config/password.php
@@ -8,27 +8,25 @@ return [
     'name'         => 'Passerby',
     'refreshToken' => [
         'cookie' => [
-            'httpOnly' => true,
+            'httpOnly' => env('COOKIE_HTTP_ONLY', true),
             'expire'   => 864000 // 864000 value will make the cookies expire in 10 days
         ],
     ],
-    'log'          => [ // log info into database, default into signal_log table
-        'info' => [
-            'login'   => [
-                'active'  => false,
-                'message' => 'User has successfully login.',
-            ],
-            'refresh' => [
-                'active'  => false,
-                'message' => 'Token refreshed.',
-            ],
-            'logout'  => [
-                'active'  => false,
-                'message' => 'User has successfully logout.',
-            ],
-        ],
+
+    'log' => [// log info into database, default into signal_log table
+              'info' => [
+                  'login'  => [
+                      'active'  => true,
+                      'message' => 'User has successfully login.',
+                  ],
+                  'logout' => [
+                      'active'  => true,
+                      'message' => 'User has successfully logout.',
+                  ],
+              ],
     ],
-    'client'       => [
+
+    'client' => [
         'id'     => env('PASSWORD_CLIENT_ID'),
         'secret' => env('PASSWORD_CLIENT_SECRET'),
     ],
