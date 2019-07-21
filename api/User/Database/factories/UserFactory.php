16a4d09e5c3d0edbad7855f602bd5db0340e5827
anonymoussc <50c5ac69@opayq.com>
2019-07-22 04:52:18 +0700

    feat(api\user): new api/User/Database/factories/UserFactory

---
+++ api/User/Database/factories/UserFactory.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * UserFactory.php
+ * Created by @anonymoussc on 07/22/2019 4:41 AM.
+ */
+
+/**
+ * Copyright(c) 2019. All rights reserved.
+ * Last modified 7/22/19 4:43 AM
+ */
+
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(Api\User\Entities\User::class, function(Faker $faker) {
+    return [
+        'name'              => $faker->name,
+        'email'             => $faker->unique()->safeEmail,
+        'email_verified_at' => now(),
+        'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'remember_token'    => Str::random(10),
+    ];
+});
